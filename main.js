d0d1a0d33968e3159ff4beba0f291c5539abd336
Hoang-UIT <trinhphuochoang@gmail.com>
2020-06-05 11:30:27 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -47,6 +47,7 @@ const peer = new Peer({
 
 
 peer.on('open',id => {
+	console.log("connect socket");
 	$('#my-peer').append(id);
 	$('#btnSignUp').click(()=> {
 		const username = $('#txtUsername').val();
